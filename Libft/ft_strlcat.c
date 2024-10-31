6c71c8961e7d23579f160064fdc94350dd5caf5a
Stegen <152444769+StegenX@users.noreply.github.com>
2024-10-31 18:37:29 +0100

    Update ft_strlcat.c

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -18,14 +18,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	len;
 	size_t	d_len;
 
-	len = strlen(dst) + strlen(src);
+	len = ft_strlen(dst) + ft_strlen(src);
 	i = 0;
-	if (size <= strlen(dst))
+	if (size <= ft_strlen(dst))
 	{
 		return (len);
 	}
-	d_len = strlen(dst);
-	while ((i < size - (len- strlen(src)) && src[i]))
+	d_len = ft_strlen(dst);
+	while ((i < size - (len- ft_strlen(src)) && src[i]))
 	{
 		dst[d_len] = src[i];
 		i++;
