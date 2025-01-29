0394b307bd287899ff4e97b8a2fe5a4f41ff9672
Stegen <152444769+StegenX@users.noreply.github.com>
2025-01-29 16:13:29 +0100

    Add files via upload

---
+++ minitalk/server.c
@@ -0,0 +1,36 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+
+static void handle_signal(int signum, siginfo_t *info, void *context) {
+    static char c = 0;
+    static int bit_count = 0;
+
+    if (signum == SIGUSR1)
+        c |= (0 << bit_count);
+    else if (signum == SIGUSR2)
+        c |= (1 << bit_count);
+
+    bit_count++;
+    if (bit_count == 8) {
+        write(1, &c, 1);
+        c = 0;
+        bit_count = 0;
+    }
+    kill(info->si_pid, SIGUSR1);
+}
+
+int main() 
+{
+    struct sigaction sa;
+    sa.sa_sigaction = handle_signal;
+    sa.sa_flags = SA_SIGINFO;
+
+    sigaction(SIGUSR1, &sa, NULL);
+    sigaction(SIGUSR2, &sa, NULL);
+
+    printf("Server PID: %d\n", getpid());
+    while (1)
+        pause();
+    return 0;
+}
