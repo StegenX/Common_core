13489fbd7d59f8bbd8b5afe4d99630e9d49fc128
Stegen <152444769+StegenX@users.noreply.github.com>
2025-01-29 18:14:31 +0100

    Add files via upload

---
+++ minitalk/client.c
@@ -0,0 +1,90 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void	handler(int sig)
+{
+	if (sig == SIGUSR1)
+		printf("HeHe");
+	else
+		printf("Pehehehe");
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	nb;
+	int	sign;
+
+	nb = 0;
+	sign = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr++ == '-')
+			sign = -1;
+	}
+	while (*nptr >= 48 && *nptr <= 57)
+	{
+		nb = nb * 10 + (*nptr - 48);
+		nptr++;
+	}
+	return (nb * sign);
+}
+
+void	send_null(int PID)
+{
+	int	i;
+
+	i = 0;
+	while (i < 8)
+	{
+		kill(PID, SIGUSR1);
+		i++;
+		usleep(100);
+	}
+}
+
+void	send_Signal(int PID, char *message)
+{
+	char	c;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (message[i])
+	{
+		c = message[i];
+		j = 0;
+		while (j < 8)
+		{
+			if ((c >> j) & 1)
+				kill(PID, SIGUSR2);
+			else
+				kill(PID, SIGUSR1);
+			usleep(500);
+			j++;
+		}
+		i++;
+	}
+	send_null(PID);
+}
+
+int	main(int ac, char **av)
+{
+	int pid;
+	if (ac != 3)
+	{
+		write(2, "Error\n", 6);
+		return (0);
+	}
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	pid = ft_atoi(av[1]);
+	if (pid <= 0)
+	{
+		write(2, "Invalid PID\n", 12);
+		return (0);
+	}
+	send_Signal(pid, av[2]);
+}
